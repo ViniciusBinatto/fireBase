ae35d01958b2acd4d36b2333231cf4ad8bd832c4
ViniciusBinatto <viniciusbinatto10@gmail.com>
2026-04-15 20:28:55 -0300

    first commit

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDCmRgRZOHc4YTUqWhK67G-iFLVW4riZr4",
+  authDomain: "dsjquery-7c3d7.firebaseapp.com",
+  projectId: "dsjquery-7c3d7",
+  storageBucket: "dsjquery-7c3d7.firebasestorage.app",
+  messagingSenderId: "355432022943",
+  appId: "1:355432022943:web:cac05427721fabbcad33aa",
+  measurementId: "G-496JEDVGYP"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
